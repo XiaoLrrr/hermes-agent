Gdheubs/hermes

fix(custom): add providers.<name>.reasoning_format to control the reasoning wire shape
provider=custom endpoints disagree on how reasoning controls are spelled
on the wire: GLM/ARK/DashScope read a top-level reasoning_effort string,
OpenRouter-style gateways expect a nested extra_body.reasoning object,
and some strict proxies reject either unknown field with a 400.
CustomProfile unconditionally sent the top-level string, so object-dialect
endpoints rejected every reasoning-enabled request (#72649).

Add an opt-in providers.<name>.reasoning_format key with three values:
top_level (default; unset keeps the historical wire shape byte-for-byte),
reasoning_object (extra_body.reasoning = {enabled: ..., effort: ...}),
and none (no reasoning fields at all — also fixes /reasoning none
still 400ing on strict proxies because the disable path sent
reasoning_effort + think). Unknown values fail closed to the default.

Repro (before this change):
1. Register an OpenRouter-style gateway as provider=custom
   (providers: {gw: {base_url: http://127.0.0.1:8317/v1}})
2. Set model.reasoning_effort: ultra and start a chat
3. Request carries top-level reasoning_effort=ultra
4. Gateway rejects it: level ultra not supported, valid levels: low, medium, high
5. Same request succeeds when reasoning is sent as
   extra_body.reasoning={enabled:true,effort:ultra} —
   now reachable via reasoning_format: reasoning_object

Fixes #72649

tests/plugins/model_providers/test_custom_profile.py
@@ -13,6 +13,9 @@ These tests pin the wire-shape contract:
                           including ``max``/``xhigh``
   - enabled + no effort → nothing emitted (endpoint's server default applies)
   - ollama_num_ctx      → extra_body.options.num_ctx, orthogonal to reasoning
+  - providers.<name>.reasoning_format overrides the dialect per endpoint
+    (#72649): "reasoning_object" nests extra_body.reasoning={enabled, effort},
+    "none" emits no reasoning fields, "top_level"/unset keeps the above.
 """
 
 from __future__ import annotations
@@ -105,5 +108,184 @@ class TestCustomReasoningWithNumCtx:
             reasoning_config=None, ollama_num_ctx=8192, model="qwen3"
         )
         assert eb == {"options": {"num_ctx": 8192}}
+        assert tl == {}
+
+
+class TestCustomReasoningFormatOverride:
+    """``providers.<name>.reasoning_format`` switches the reasoning wire dialect.
+
+    Custom endpoints disagree on how reasoning is spelled on the wire
+    (#72649): GLM/ARK read a top-level ``reasoning_effort`` string,
+    OpenRouter-style gateways expect a nested ``reasoning`` object, and some
+    proxies 400 on either unknown field. The config lookup is monkeypatched
+    at its import site so these tests pin only the profile's dispatch; the
+    classes above run with no lookup result at all, which is the
+    byte-for-byte-unchanged guarantee for existing configs.
+    """
+
+    BASE_URL = "http://127.0.0.1:8317/v1"
+
+    def _patch_format(self, monkeypatch, fmt):
+        """Route the profile's lazy config lookup to a canned answer."""
+        calls: list = []
+
+        def fake_lookup(base_url, custom_providers=None, config=None):
+            calls.append(base_url)
+            return fmt
+
+        monkeypatch.setattr(
+            "hermes_cli.config.get_custom_provider_reasoning_format",
+            fake_lookup,
+        )
+        return calls
+
+    def test_reasoning_object_enabled_nests_in_extra_body(
+        self, custom_profile, monkeypatch
+    ):
+        """reasoning_object + enabled → extra_body.reasoning object, nothing top-level.
+
+        This is the OpenRouter-style dialect. Efforts pass through verbatim —
+        gateways that accept the object shape validate effort themselves
+        (e.g. accept "ultra" that they reject as top-level reasoning_effort).
+        """
+        self._patch_format(monkeypatch, "reasoning_object")
+        eb, tl = custom_profile.build_api_kwargs_extras(
+            reasoning_config={"enabled": True, "effort": "ultra"},
+            model="glm-5.2",
+            base_url=self.BASE_URL,
+        )
+        assert eb == {"reasoning": {"enabled": True, "effort": "ultra"}}
+        assert tl == {}
+
+    def test_reasoning_object_disabled_sends_enabled_false(
+        self, custom_profile, monkeypatch
+    ):
+        """reasoning_object + disabled → {"enabled": False}, no think/effort fields."""
+        self._patch_format(monkeypatch, "reasoning_object")
+        eb, tl = custom_profile.build_api_kwargs_extras(
+            reasoning_config={"enabled": False},
+            model="glm-5.2",
+            base_url=self.BASE_URL,
+        )
+        assert eb == {"reasoning": {"enabled": False}}
+        assert tl == {}
+
+    def test_reasoning_object_effort_none_alias_disables(
+        self, custom_profile, monkeypatch
+    ):
+        """effort='none' is the disable alias in every dialect."""
+        self._patch_format(monkeypatch, "reasoning_object")
+        eb, tl = custom_profile.build_api_kwargs_extras(
+            reasoning_config={"enabled": True, "effort": "none"},
+            model="glm-5.2",
+            base_url=self.BASE_URL,
+        )
+        assert eb == {"reasoning": {"enabled": False}}
+        assert tl == {}
+
+    def test_format_none_enabled_emits_nothing(self, custom_profile, monkeypatch):
+        """reasoning_format='none' suppresses reasoning fields on enable.
+
+        For proxies (LiteLLM-style) that 400 on any unrecognized reasoning
+        field — the endpoint's server-side default applies.
+        """
+        self._patch_format(monkeypatch, "none")
+        eb, tl = custom_profile.build_api_kwargs_extras(
+            reasoning_config={"enabled": True, "effort": "high"},
+            model="glm-5.2",
+            base_url=self.BASE_URL,
+        )
+        assert eb == {}
+        assert tl == {}
+
+    def test_format_none_disabled_emits_nothing(self, custom_profile, monkeypatch):
+        """reasoning_format='none' also suppresses the disable fields.
+
+        Fixes the second failure mode in #72649: `/reasoning none` used to
+        400 on strict proxies because the disable path still sent
+        reasoning_effort + think.
+        """
+        self._patch_format(monkeypatch, "none")
+        eb, tl = custom_profile.build_api_kwargs_extras(
+            reasoning_config={"enabled": False},
+            model="glm-5.2",
+            base_url=self.BASE_URL,
+        )
+        assert eb == {}
+        assert tl == {}
+
+    def test_explicit_top_level_matches_default_enabled(
+        self, custom_profile, monkeypatch
+    ):
+        """reasoning_format='top_level' is the spelled-out default — identical wire."""
+        self._patch_format(monkeypatch, "top_level")
+        eb, tl = custom_profile.build_api_kwargs_extras(
+            reasoning_config={"enabled": True, "effort": "high"},
+            model="glm-5.2",
+            base_url=self.BASE_URL,
+        )
+        assert eb == {}
+        assert tl == {"reasoning_effort": "high"}
+
+    def test_explicit_top_level_matches_default_disabled(
+        self, custom_profile, monkeypatch
+    ):
+        self._patch_format(monkeypatch, "top_level")
+        eb, tl = custom_profile.build_api_kwargs_extras(
+            reasoning_config={"enabled": False},
+            model="glm-5.2",
+            base_url=self.BASE_URL,
+        )
+        assert eb == {"think": False}
+        assert tl == {"reasoning_effort": "none"}
+
+    def test_no_configured_format_falls_back_to_top_level(
+        self, custom_profile, monkeypatch
+    ):
+        """Lookup returning None (no providers entry / no key) → default dialect."""
+        calls = self._patch_format(monkeypatch, None)
+        eb, tl = custom_profile.build_api_kwargs_extras(
+            reasoning_config={"enabled": True, "effort": "high"},
+            model="glm-5.2",
+            base_url=self.BASE_URL,
+        )
+        assert eb == {}
+        assert tl == {"reasoning_effort": "high"}
+        assert calls == [self.BASE_URL]
+
+    def test_lookup_failure_falls_back_to_top_level(
+        self, custom_profile, monkeypatch
+    ):
+        """A crashing config lookup must never break the request build."""
+
+        def boom(base_url, custom_providers=None, config=None):
+            raise RuntimeError("config unavailable")
+
+        monkeypatch.setattr(
+            "hermes_cli.config.get_custom_provider_reasoning_format", boom
+        )
+        eb, tl = custom_profile.build_api_kwargs_extras(
+            reasoning_config={"enabled": True, "effort": "high"},
+            model="glm-5.2",
+            base_url=self.BASE_URL,
+        )
+        assert eb == {}
+        assert tl == {"reasoning_effort": "high"}
+
+    def test_reasoning_object_composes_with_num_ctx(
+        self, custom_profile, monkeypatch
+    ):
+        """num_ctx wiring is orthogonal to the reasoning dialect."""
+        self._patch_format(monkeypatch, "reasoning_object")
+        eb, tl = custom_profile.build_api_kwargs_extras(
+            reasoning_config={"enabled": True, "effort": "medium"},
+            ollama_num_ctx=8192,
+            model="qwen3",
+            base_url=self.BASE_URL,
+        )
+        assert eb == {
+            "options": {"num_ctx": 8192},
+            "reasoning": {"enabled": True, "effort": "medium"},
+        }
         assert tl == {}
 

XiaoLrrr/hermes-agent

tests/plugins/model_providers/test_custom_profile.py
@@ -14,6 +14,9 @@ These tests pin the wire-shape contract:
                           including ``max``/``xhigh``
     - enabled + no effort → nothing emitted (endpoint's server default applies)
     - ollama_num_ctx      → extra_body.options.num_ctx, orthogonal to reasoning
+    - providers.<name>.reasoning_format overrides the dialect per endpoint
+      (#72649): "reasoning_object" nests extra_body.reasoning={enabled, effort},
+      "none" emits no reasoning fields, "top_level"/unset keeps the above.
 """
 
 from __future__ import annotations
@@ -180,3 +183,181 @@ class TestCustomReasoningWithNumCtx:
         assert eb == {"options": {"num_ctx": 8192}}
         assert tl == {}
 
+
+class TestCustomReasoningFormatOverride:
+    """``providers.<name>.reasoning_format`` switches the reasoning wire dialect.
+
+    Custom endpoints disagree on how reasoning is spelled on the wire
+    (#72649): GLM/ARK read a top-level ``reasoning_effort`` string,
+    OpenRouter-style gateways expect a nested ``reasoning`` object, and some
+    proxies 400 on either unknown field. The config lookup is monkeypatched
+    at its import site so these tests pin only the profile's dispatch; the
+    classes above run with no lookup result at all, which is the
+    byte-for-byte-unchanged guarantee for existing configs.
+    """
+
+    BASE_URL = "http://127.0.0.1:8317/v1"
+
+    def _patch_format(self, monkeypatch, fmt):
+        """Route the profile's lazy config lookup to a canned answer."""
+        calls: list = []
+
+        def fake_lookup(base_url, custom_providers=None, config=None):
+            calls.append(base_url)
+            return fmt
+
+        monkeypatch.setattr(
+            "hermes_cli.config_providers.get_custom_provider_reasoning_format",
+            fake_lookup,
+        )
+        return calls
+
+    def test_reasoning_object_enabled_nests_in_extra_body(
+        self, custom_profile, monkeypatch
+    ):
+        """reasoning_object + enabled → extra_body.reasoning object, nothing top-level.
+
+        This is the OpenRouter-style dialect. Efforts pass through verbatim —
+        gateways that accept the object shape validate effort themselves
+        (e.g. accept "ultra" that they reject as top-level reasoning_effort).
+        """
+        self._patch_format(monkeypatch, "reasoning_object")
+        eb, tl = custom_profile.build_api_kwargs_extras(
+            reasoning_config={"enabled": True, "effort": "ultra"},
+            model="glm-5.2",
+            base_url=self.BASE_URL,
+        )
+        assert eb == {"reasoning": {"enabled": True, "effort": "ultra"}}
+        assert tl == {}
+
+    def test_reasoning_object_disabled_sends_enabled_false(
+        self, custom_profile, monkeypatch
+    ):
+        """reasoning_object + disabled → {"enabled": False}, no think/effort fields."""
+        self._patch_format(monkeypatch, "reasoning_object")
+        eb, tl = custom_profile.build_api_kwargs_extras(
+            reasoning_config={"enabled": False},
+            model="glm-5.2",
+            base_url=self.BASE_URL,
+        )
+        assert eb == {"reasoning": {"enabled": False}}
+        assert tl == {}
+
+    def test_reasoning_object_effort_none_alias_disables(
+        self, custom_profile, monkeypatch
+    ):
+        """effort='none' is the disable alias in every dialect."""
+        self._patch_format(monkeypatch, "reasoning_object")
+        eb, tl = custom_profile.build_api_kwargs_extras(
+            reasoning_config={"enabled": True, "effort": "none"},
+            model="glm-5.2",
+            base_url=self.BASE_URL,
+        )
+        assert eb == {"reasoning": {"enabled": False}}
+        assert tl == {}
+
+    def test_format_none_enabled_emits_nothing(self, custom_profile, monkeypatch):
+        """reasoning_format='none' suppresses reasoning fields on enable.
+
+        For proxies (LiteLLM-style) that 400 on any unrecognized reasoning
+        field — the endpoint's server-side default applies.
+        """
+        self._patch_format(monkeypatch, "none")
+        eb, tl = custom_profile.build_api_kwargs_extras(
+            reasoning_config={"enabled": True, "effort": "high"},
+            model="glm-5.2",
+            base_url=self.BASE_URL,
+        )
+        assert eb == {}
+        assert tl == {}
+
+    def test_format_none_disabled_emits_nothing(self, custom_profile, monkeypatch):
+        """reasoning_format='none' also suppresses the disable fields.
+
+        Fixes the second failure mode in #72649: `/reasoning none` used to
+        400 on strict proxies because the disable path still sent
+        reasoning_effort + think.
+        """
+        self._patch_format(monkeypatch, "none")
+        eb, tl = custom_profile.build_api_kwargs_extras(
+            reasoning_config={"enabled": False},
+            model="glm-5.2",
+            base_url=self.BASE_URL,
+        )
+        assert eb == {}
+        assert tl == {}
+
+    def test_explicit_top_level_matches_default_enabled(
+        self, custom_profile, monkeypatch
+    ):
+        """reasoning_format='top_level' is the spelled-out default — identical wire."""
+        self._patch_format(monkeypatch, "top_level")
+        eb, tl = custom_profile.build_api_kwargs_extras(
+            reasoning_config={"enabled": True, "effort": "high"},
+            model="glm-5.2",
+            base_url=self.BASE_URL,
+        )
+        assert eb == {}
+        assert tl == {"reasoning_effort": "high"}
+
+    def test_explicit_top_level_matches_default_disabled(
+        self, custom_profile, monkeypatch
+    ):
+        self._patch_format(monkeypatch, "top_level")
+        eb, tl = custom_profile.build_api_kwargs_extras(
+            reasoning_config={"enabled": False},
+            model="glm-5.2",
+            base_url=self.BASE_URL,
+        )
+        assert eb == {}
+        assert tl == {"reasoning_effort": "none"}
+
+    def test_no_configured_format_falls_back_to_top_level(
+        self, custom_profile, monkeypatch
+    ):
+        """Lookup returning None (no providers entry / no key) → default dialect."""
+        calls = self._patch_format(monkeypatch, None)
+        eb, tl = custom_profile.build_api_kwargs_extras(
+            reasoning_config={"enabled": True, "effort": "high"},
+            model="glm-5.2",
+            base_url=self.BASE_URL,
+        )
+        assert eb == {}
+        assert tl == {"reasoning_effort": "high"}
+        assert calls == [self.BASE_URL]
+
+    def test_lookup_failure_falls_back_to_top_level(
+        self, custom_profile, monkeypatch
+    ):
+        """A crashing config lookup must never break the request build."""
+
+        def boom(base_url, custom_providers=None, config=None):
+            raise RuntimeError("config unavailable")
+
+        monkeypatch.setattr(
+            "hermes_cli.config_providers.get_custom_provider_reasoning_format", boom
+        )
+        eb, tl = custom_profile.build_api_kwargs_extras(
+            reasoning_config={"enabled": True, "effort": "high"},
+            model="glm-5.2",
+            base_url=self.BASE_URL,
+        )
+        assert eb == {}
+        assert tl == {"reasoning_effort": "high"}
+
+    def test_reasoning_object_composes_with_num_ctx(
+        self, custom_profile, monkeypatch
+    ):
+        """num_ctx wiring is orthogonal to the reasoning dialect."""
+        self._patch_format(monkeypatch, "reasoning_object")
+        eb, tl = custom_profile.build_api_kwargs_extras(
+            reasoning_config={"enabled": True, "effort": "medium"},
+            ollama_num_ctx=8192,
+            model="qwen3",
+            base_url=self.BASE_URL,
+        )
+        assert eb == {
+            "options": {"num_ctx": 8192},
+            "reasoning": {"enabled": True, "effort": "medium"},
+        }
+        assert tl == {}